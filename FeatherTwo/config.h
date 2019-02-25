12aa55e1650a17781439caca546c5803770930c5
olgamedt <andicsson@gmail.com>
2019-02-25 14:42:28 +0100

    Lägger till filer
    Lägger till filer som inte fanns från början

---
+++ FeatherTwo/config.h
@@ -0,0 +1,30 @@
+// Physical device information for board and sensor
+#define DEVICE_ID             WiFi.macAddress()     // already configured
+#define DEVICE_STUDENT        "Tolga"
+#define DEVICE_TYPE           "tempSensor"
+#define DEVICE_LOCATION_LAT   "59.390548"
+#define DEVICE_LOCATION_LON   "17.923066"      
+#define DEVICE_SEND_INTERVAL 60000           //default 3600000 ms = 1 hour
+
+#define WIFI_SSID             "IoT"
+#define WIFI_PASS             "IoT2018!"
+
+//#define WIFI_SSID             "IoT"
+//#define WIFI_PASS             "IoT2018!"
+
+
+//DHT Temperature sensor configuration
+#define DHT_PIN               2
+#define DHT_TYPE              DHT11
+
+
+// Pin layout configuration
+#define LED_PIN               0
+
+
+// Address configuration, don't remove!
+#define MESSAGE_MAX_LEN       256
+#define EEPROM_SIZE           512
+#define SSID_LEN              32
+#define PASS_LEN              32
+#define CONNECTIONSTRING_LEN  256
